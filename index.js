ceeca2e18199ad32fff782e30976a95204b42347
Noxxinoxx <nooaanderssondator@gmail.com>
2022-08-28 04:07:35 +0200

    fast change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const https = require('https');
 const fs = require('fs');
 var express = require("express")
 const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem')
 };
 var app = express()
 var server = https.createServer(options, app)
